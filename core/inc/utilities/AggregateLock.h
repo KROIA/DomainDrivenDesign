9000f26c46c8d66e0af2ec63d77cb06b4529737c
Alex <alexkrieg@gmx.ch>
2026-01-19 17:04:28 +0100

    -

--- core/inc/utilities/AggregateLock.h
+++ core/inc/utilities/AggregateLock.h
@@ -7,8 +7,18 @@ namespace DDD
 	class AggregateLock
 	{
 	public:
+		AggregateLock() = default;
+		AggregateLock(const AggregateLock&) = default;
+		AggregateLock(AggregateLock&&) noexcept = default;
 		virtual ~AggregateLock() = default;
 
 		virtual std::string toString() = 0;
+
+		virtual void setAggregateID(const DDD::ID& ref) { m_lockedAggregateID = ref; }
+		const DDD::ID& getAggregateID() const { return m_lockedAggregateID; }
+
+
+	private:
+		DDD::ID m_lockedAggregateID;
 	};
 }
